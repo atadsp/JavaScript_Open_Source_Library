4a20590037e08fdebfaf500f3b16d8e4dcd64b02
Chris Beckman <atadsp@gmail.com>
2016-07-14 11:55:54 -0400

    added an array into the object type

--- 1.72_objects_inheritance.js
+++ 1.72_objects_inheritance.js
@@ -1,25 +1,51 @@
 // JavaScript Document
 //redid the object as a function so I could create children more easily.
+//all children of the monster object have the same base propteries, but those properties contain diffrent values
 function monster(name, age, height, weight) {
     this.name = name;
     this.age = age;
     this.height = height;
     this.weight = weight;
+    
+    
+	this.abilties = [];
+			
+			
 	this.size = function() {
-		console.log(this.height + " meters, and " + this.weight + " kilograms!");};
+		console.log(this.height + " meters, and " + this.weight + " kilograms!");
+	};
+		
 	this.nameChange = function(newName){
 		this.name = newName;
 		return this.name;
-	}
+	};
+	
+	this.listAbilties = function(){
+		for (i = 0; this.abilties.length > i; i++) { 
+			console.log(this.name + " has " +this.abilties[i]);
+		}
+	};
 }
 
 //create two children, Godzilla and Rodan, each has the same properties
 
 var godzilla = new monster("Godzilla", 65000000, 108, 95000);
+
+godzilla.abilties.push("Super Strength");
+godzilla.abilties.push("Super Endurance");
+godzilla.abilties.push("Super Regeneration");
+godzilla.abilties.push("Atomic Breath");
+godzilla.abilties.push("Nuclear Pulse");
+
+
 var rodan = new monster("Rodan", 66, 100, 30000);
+rodan.abilties.push("Flight at Mach 3");
 
 //I can also run the same functions on both easily
 godzilla.size();
 rodan.size();
 godzilla.nameChange('Gojira');
 rodan.nameChange('Redon');
+
+godzilla.listAbilties();
+rodan.listAbilties();
