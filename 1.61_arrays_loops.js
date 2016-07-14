cc871e79cbb40d058955b09364bdca58462fc167
Chris Beckman <atadsp@gmail.com>
2016-07-14 10:35:40 -0400

    looping over an array

--- 1.61_arrays_loops.js
+++ 1.61_arrays_loops.js
@@ -1,1 +1,10 @@
 // JavaScript Document
+var people = ["chris","paul","joey","justin"]
+
+for (i =0; i<people.length; i++){
+	if(i%2 ==0) {
+		console.log(people[i] + " is amazing");
+	} else{
+		console.log(people[i] + " is okay");
+	}
+}
