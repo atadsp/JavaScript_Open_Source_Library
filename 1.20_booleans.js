da973ab8102aebd54cbade1e73ddd057297c3704
Chris Beckman <atadsp@gmail.com>
2016-07-14 09:55:40 -0400

    booleans = true

--- 1.20_booleans.js
+++ 1.20_booleans.js
@@ -1,1 +1,12 @@
 // JavaScript Document
+
+var name="chris";
+
+(10 > 9); //returns true
+(9>19); //returns false
+
+name === "chris";
+
+var num = 'ten';
+
+num == 10; //returns false since they are not the same data type
