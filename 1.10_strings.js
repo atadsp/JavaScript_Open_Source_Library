04ceae286084def6bbb728dd638d0af612c44b7f
Chris Beckman <atadsp@gmail.com>
2016-07-14 09:48:15 -0400

    Strings are done

--- 1.10_strings.js
+++ 1.10_strings.js
@@ -1,1 +1,22 @@
-// JavaScript Document
+var testString = "Oh what a humiliting defeat, I am beat";
+var extraString = "Wont you please have a seat";
+
+//concatinating two string with a space in between
+console.log(testString + " " + extraString);
+
+var funLine = "Why dont you drop dead Fred?!"
+var reflection = "My expectations were higher"
+
+//upper case
+console.log(funLine.toUpperCase());
+//lower case
+console.log(reflection.toLowerCase());
+
+//bounce off the length of a string
+console.log(reflection.length);
+
+//return a specific letter at an index
+console.log(reflection.charAt(4));
+
+var trim = "      Hello, this should work         ";
+console.log(trim.trim());
