763581dd3faca7d035ac72de4dee9de7cbf84f44
Chris Beckman <atadsp@gmail.com>
2016-07-14 09:37:08 -0400

    modified intiger file

--- 1.00_integers.js
+++ 1.00_integers.js
@@ -1,9 +1,24 @@
 // JavaScript Document
 
+//basic intigers
 var x = 44;
 var y = 23;
 var z = x - y;
-var w = (x*y)/z
+var w = (x*y)/z;
 
 console.log(z);
 console.log(w);
+
+
+//add one, and subtract one
+x ++;
+
+y --;
+
+//add or subtract more than one
+x -= 10;
+x += 10;
+
+//modulas 
+console.log(x%y);
+
