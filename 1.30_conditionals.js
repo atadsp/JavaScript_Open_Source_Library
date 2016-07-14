59b662c12ac9d012b1e4897b0641f62f95559ae0
Chris Beckman <atadsp@gmail.com>
2016-07-14 10:02:47 -0400

    conditionals = ture

--- 1.30_conditionals.js
+++ 1.30_conditionals.js
@@ -1,1 +1,22 @@
 // JavaScript Document
+
+var chrisIsAwesome =true;
+var chrisHatesCode =false;
+var age = 24;
+
+if(chrisIsAwesome === true){
+	console.log("Damn Skippy");
+} else if (chrisHatesCode === true){
+	console.log("That's not true, thats impossible");
+}
+
+if (chrisHatesCode === false){
+	console.log("Code is love. Code is life.")
+} else {
+	chrisIsAwesome === false;
+	console.log("Fallen from grace")
+}
+
+if (chrisHatesCode === false && age >= 21){
+	console.log("This demonstrates using two diffrent factors to determine something");
+}
