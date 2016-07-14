e5e522c6ef94b2b02bc9158e818bfd6abe3f27aa
Chris Beckman <atadsp@gmail.com>
2016-07-14 10:09:12 -0400

    functions complete

--- 1.40_functions.js
+++ 1.40_functions.js
@@ -1,1 +1,20 @@
 // JavaScript Document
+
+function example (cheat,fun,creative){
+	if(cheat===true){
+		console.log("I am shocked");
+	} else {
+		console.log("Figures");
+	}
+	if(fun ===true){
+		console.log("Coding is amazing")
+	} else if (cheat === true && fun === false){
+		console.log("You were the chosen one")
+	}
+	if(creative===false){
+		console.log("Examples are hard")
+	} else {
+		console.log("I am a scrum lord")
+	}
+}
+example(false,true,false)
