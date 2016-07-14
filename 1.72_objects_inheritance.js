05d3709580a741913eabe5486c7434bbacacba3e
Chris Beckman <atadsp@gmail.com>
2016-07-14 11:11:37 -0400

    Inheritance example along with object construction

--- 1.72_objects_inheritance.js
+++ 1.72_objects_inheritance.js
@@ -1,1 +1,25 @@
 // JavaScript Document
+//redid the object as a function so I could create children more easily.
+function monster(name, age, height, weight) {
+    this.name = name;
+    this.age = age;
+    this.height = height;
+    this.weight = weight;
+	this.size = function() {
+		console.log(this.height + " meters, and " + this.weight + " kilograms!");};
+	this.nameChange = function(newName){
+		this.name = newName;
+		return this.name;
+	}
+}
+
+//create two children, Godzilla and Rodan, each has the same properties
+
+var godzilla = new monster("Godzilla", 65000000, 108, 95000);
+var rodan = new monster("Rodan", 66, 100, 30000);
+
+//I can also run the same functions on both easily
+godzilla.size();
+rodan.size();
+godzilla.nameChange('Gojira');
+rodan.nameChange('Redon');
