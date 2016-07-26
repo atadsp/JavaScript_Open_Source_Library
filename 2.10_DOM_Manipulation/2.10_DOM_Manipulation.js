b8d808d22636ffb81ac13843f9072747b74c8dba
Chris Beckman <atadsp@gmail.com>
2016-07-26 14:47:20 -0400

    added a section on dom manipulation built into vanilla javascript

--- 2.10_DOM_Manipulation/2.10_DOM_Manipulation.js
+++ 2.10_DOM_Manipulation/2.10_DOM_Manipulation.js
@@ -1,1 +1,14 @@
 // JavaScript Document
+
+//using javascript you can manipulate the front end of the website to add interactivity to an otherwise static webpage. 
+//by using the  document.getElementById function you can add or remove or collect data from HTML elements with the corrisponding ID
+
+//this is an event listener that is listening for a click
+window.onclick = myFunction;
+
+// If the user clicks in the window, it will add the text to the paragraph tag with the corrisponding id tag
+function myFunction() {
+	document.getElementById('output').innerHTML += ("This information will be appended to the paragraph tag");
+}
+
+//if you include Jquery you can get even more intrict with its functionality
